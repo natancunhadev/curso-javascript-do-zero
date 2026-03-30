3d128c8e5202b791ce01674167a4893c7e7aec1a
Natan de Souza Cunha <nncunha48@gmail.com>
2026-03-30 11:41:17 -0300

    2026.02
    Multipli.js

--- multipli.js
+++ multipli.js
@@ -1,9 +1,9 @@
-const numbers = [60, 20,10];
+const numbers = [60 * 20 * 10];
 
 const sum = numbers.reduce((acc, curr) => acc + curr, 0);
 
 console.log(sum); 
 
 
-// Faça uma soma com resultado output 90. curso javascriptdozero!
+// Faça uma multiplicação com resultado output 12000. curso javascriptdozero!
 // Natan de Souza Cunha - Software Enginnering 2026
