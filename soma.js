f85838ff8d6d3bc7f8f27bd70b4eecb822a05129
Natan de Souza Cunha <nncunha48@gmail.com>
2026-03-30 09:25:32 -0300

    2026.01
    Soma.js

---
+++ soma.js
@@ -0,0 +1,9 @@
+const numbers = [60, 20,10];
+
+const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sum); 
+
+
+// faça uma soma com resultado output 90. curso javascriptdozero!
+// Natan de Souza Cunha - Software Enginnering 2026
